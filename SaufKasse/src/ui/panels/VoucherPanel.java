573cdbd4ed806245a67592f7970e4cf5710b189d
Marian <Marian@MWeitz-Tab>
2016-07-07 12:00:07 +0200

    delete System.out.println

--- SaufKasse/src/ui/panels/VoucherPanel.java
+++ SaufKasse/src/ui/panels/VoucherPanel.java
@@ -32,7 +32,6 @@ public class VoucherPanel extends AbstractKassenPanel {
 	public void updateVoucherCount(Map<Integer, Integer> voucherMap) {
 		
 		for (int i = 0; i < voucherLabels.size(); i++) {
-			System.out.println(voucherLabels.size());
 			Integer vIDCount = voucherMap.get(i + 1);
 			if (vIDCount == null) {
 				vIDCount = 0;
