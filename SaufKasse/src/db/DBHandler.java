7116351a5fe05e40aa01967a766e9b043dc03d1e
Marian <Marian@MWeitz-Tab>
2016-07-07 09:22:31 +0200

    small changes before merge in master

--- SaufKasse/src/db/DBHandler.java
+++ SaufKasse/src/db/DBHandler.java
@@ -8,7 +8,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.logging.Logger;
 
 import beans.Invoice;
 import beans.Product;
--- SaufKasse/src/utilities/Variables.java
+++ SaufKasse/src/utilities/Variables.java
@@ -6,5 +6,5 @@ public class Variables {
 
 	public static Font buttonAndComboFont = new Font("Arial", Font.BOLD, 30); 
 	public static Font menuFont = new Font("Arial", Font.PLAIN, 15); 	
-
+	
 }
