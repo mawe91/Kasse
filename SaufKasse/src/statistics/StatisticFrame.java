9d639530ff00bc6b2fc4fb8b631e6f44e885e1a6
Marian <Marian@MWeitz-Tab.fritz.box>
2016-07-16 12:35:30 +0200

    change statistic pane to JTabbedPane for multiple statistics

--- SaufKasse/src/statistics/StatisticFrame.java
+++ SaufKasse/src/statistics/StatisticFrame.java
@@ -1,21 +1,25 @@
 package statistics;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.Panel;
 
 import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.CategoryPlot;
-import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.chart.plot.PlotOrientation;
 import model.Model;
-
 
 public class StatisticFrame extends JFrame {
 
@@ -23,36 +27,49 @@ public class StatisticFrame extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private Panel panel;
+	private Panel panel1;
+	private Panel panel2;
+	
 	private Container c;
 	private Model model;
 	
-	public StatisticFrame (Model model){
-		super ("Statistiken");
+
+	public StatisticFrame(Model model) {
+		super("Statistiken");
+		
+		JTabbedPane tabbedPane = new JTabbedPane();
 
 		this.model = model;
-		panel = new Panel();
-		
+		panel1 = new Panel(new BorderLayout());
+		panel2 = new Panel(new BorderLayout());
+
 		c = getContentPane();
-		c.setLayout(new FlowLayout());
+		c.setLayout(new CardLayout());
 
 		setSize(new Dimension(1024, 768));
 		setVisible(true);
-		
+
 		JFreeChart chart = ChartFactory.createBarChart("Verkaufte Produkte", "", "", model.getSoldProductsDataset());
 		CategoryPlot catplot = chart.getCategoryPlot();
 		catplot.setRangeGridlinePaint(Color.BLACK);
-		
-		ChartPanel chartpanel = new ChartPanel(chart);
-		panel.removeAll();
-		panel.add(chartpanel, BorderLayout.CENTER);
-		
-		c.add(panel);
 
+		ChartPanel chartpanel1 = new ChartPanel(chart);
+		panel1.removeAll();
+		panel1.add(chartpanel1, BorderLayout.CENTER);		
+		
+		JFreeChart ch = ChartFactory.createBarChart("a", "b", "c", model.getSoldProductsDataset(),
+				PlotOrientation.HORIZONTAL, false, false, false);
+		CategoryPlot chpl = ch.getCategoryPlot();
+		chpl.setRangeGridlinePaint(Color.BLACK);		
+
+		ChartPanel chartpanel2 = new ChartPanel(ch);
+		panel2.removeAll();
+		panel2.add(chartpanel2, BorderLayout.CENTER);
+
+		tabbedPane.add("Statistik 1", panel1);
+		tabbedPane.add("Statistik 2", panel2);
+		
+		c.add(tabbedPane, BorderLayout.CENTER);
 	}
-
-	
-	
-	
 
 }
